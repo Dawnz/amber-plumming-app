bfe6a97ef40878223b3cf354e0ad6ba1c39461e6
K Gordon <krisan_gordon@yahoo.com>
2022-09-13 11:46:02 -0500

    Added Login and Registration UI with validation

---
+++ Trinity-Plumbing-App/src/app/components/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  public reactiveForm!: FormGroup;
+
+  constructor() {
+
+    this.reactiveForm = new FormGroup({
+      email : new FormControl('', Validators.required),
+      password : new FormControl('', Validators.required),
+      passwordConfirmed : new FormControl('', Validators.required),
+      userName: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(16)])
+    })
+
+   }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+
+  }
+
+}
