2810d4e884f7ce82ea7659d0f8ec425da75b75c6
K Gordon <krisan_gordon@yahoo.com>
2022-09-15 10:53:24 -0500

    added pages folder. updated store page component

--- Trinity-Plumbing-App/src/app/app-routing.module.ts
+++ Trinity-Plumbing-App/src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { RegistrationComponent } from './components/registration/registration.component';
-import { StorePageComponent } from './components/store-page/store-page.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
+import { StorePageComponent } from './pages/store-page/store-page.component';
 
 const routes: Routes = [
   {path: "login", component: LoginComponent},
--- Trinity-Plumbing-App/src/app/app.module.ts
+++ Trinity-Plumbing-App/src/app/app.module.ts
@@ -4,24 +4,26 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegistrationComponent } from './components/registration/registration.component';
-import { StorePageComponent } from './components/store-page/store-page.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
+import { StorePageComponent } from './pages/store-page/store-page.component';
 import { SlideshowComponent } from './components/slideshow/slideshow.component';
 import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ItemBoxComponent } from './components/item-box/item-box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegistrationComponent,
+    StorePageComponent,
     SlideshowComponent,
     ShoppingCartComponent,
     NavBarComponent,
     FooterComponent,
-    StorePageComponent
+    ItemBoxComponent
   ],
   imports: [
     BrowserModule,
