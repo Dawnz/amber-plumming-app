3591e4fbf549b118c9b6958fc35ebcca7a651c7a
rajeime <rajeime.m.roberts@gmail.com>
2022-09-16 09:17:30 -0500

    made changes

--- Trinity-Plumbing-App/src/app/pages/landing-page/landing-page.component.ts
+++ Trinity-Plumbing-App/src/app/pages/landing-page/landing-page.component.ts
@@ -9,21 +9,22 @@ export class LandingPageComponent implements OnInit {
 
   constructor() { }
 
+  // should be replaced with data from database and splce for only three item
   items=[
     {
       name:'Sinks',
       image:'/assets/sink.png',
-      description:'Lorem ipsum dolor sit amet consectetur adipisicing....',
+      description:'Lorem ipsum dolor sit amet consectetur adipisicing .Lorem ipsum dolor sit amet',
     },
     {
       name:'Faucets',
       image:'/assets/sink.png',
-      description:'Lorem ipsum dolor sit amet consectetur adipisicing....',
+      description:'Lorem ipsum dolor sit amet consectetur adipisicing. Lorem ipsum dolor sit amet consectetur adipisicing',
     },
     {
       name:'Bath Tubs',
       image:'/assets/sink.png',
-      description:'Lorem ipsum dolor sit amet consectetur adipisicing....',
+      description:'Lorem ipsum dolor sit amet consectetur adipisicing. Lorem ipsum dolor sit amet',
     }
   ]
 
