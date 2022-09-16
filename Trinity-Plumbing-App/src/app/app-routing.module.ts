2f9b1dadc283f48f781c12be46dbb985e6f2a368
rajeime <rajeime.m.roberts@gmail.com>
2022-09-16 01:45:41 -0500

    added link to router module

--- Trinity-Plumbing-App/src/app/app-routing.module.ts
+++ Trinity-Plumbing-App/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminRegistrationComponent } from './pages/admin-registration/admin-registration.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegistrationComponent } from './pages/registration/registration.component';
 import { StorePageComponent } from './pages/store-page/store-page.component';
 
 const routes: Routes = [
+  {path:'home', component: LandingPageComponent},
   {path: "login", component: LoginComponent},
   {path: "register", component: RegistrationComponent},
   {path: "adminRegister", component: AdminRegistrationComponent},
