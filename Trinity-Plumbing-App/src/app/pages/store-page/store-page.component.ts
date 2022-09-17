a013156091dcc3604efdcf7a054786f394e9c9bf
dgreenbluechip <dgreen@bluechipstrategies.com.jm>
2022-09-17 14:44:03 -0500

    updated to render card

--- Trinity-Plumbing-App/src/app/pages/store-page/store-page.component.ts
+++ Trinity-Plumbing-App/src/app/pages/store-page/store-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/interfaces/product';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -8,67 +9,68 @@ import { ProductService } from 'src/app/services/product.service';
 })
 export class StorePageComponent implements OnInit {
   constructor(private productsApi: ProductService) {}
+  products: Product[] = [];
 
   ngOnInit(): void {
-    this.pageSlice = this.products.slice(0, 6);
     this.productsApi.getAllProducts().subscribe((data) => {
-      console.log(data);
+      this.products = data;
+      this.pageSlice = this.products.slice(0, 6);
     });
   }
 
-  products = [
-    {
-      name: 'Kitchen Chrome',
-      img: 'https://www.ikea.com/us/en/images/products/kallsjoen-pillar-faucet-chrome-plated__0914025_pe783844_s5.jpg?f=s',
-      price: 2000,
-    },
-    {
-      name: 'Bathtub',
-      img: 'https://images.thdstatic.com/productImages/f31d4d41-70cc-4968-a135-7a975d4f670a/svn/white-hydro-systems-corner-bathtubs-stu5959atow-64_300.jpg',
-      price: 1400,
-    },
-    {
-      name: 'Shower Head',
-      img: 'https://www.vieffetrade.eu/shop/foto_articoli/Hansgrohe/527023/527023.jpg',
-      price: 2500,
-    },
-    {
-      name: 'Kitchen Chrome',
-      img: 'https://www.ikea.com/us/en/images/products/kallsjoen-pillar-faucet-chrome-plated__0914025_pe783844_s5.jpg?f=s',
-      price: 2000,
-    },
-    {
-      name: 'Bathtub',
-      img: 'https://images.thdstatic.com/productImages/f31d4d41-70cc-4968-a135-7a975d4f670a/svn/white-hydro-systems-corner-bathtubs-stu5959atow-64_300.jpg',
-      price: 1400,
-    },
-    {
-      name: 'Shower Head',
-      img: 'https://www.vieffetrade.eu/shop/foto_articoli/Hansgrohe/527023/527023.jpg',
-      price: 2500,
-    },
+  // products = [
+  //   {
+  //     name: 'Kitchen Chrome',
+  //     img: 'https://www.ikea.com/us/en/images/products/kallsjoen-pillar-faucet-chrome-plated__0914025_pe783844_s5.jpg?f=s',
+  //     price: 2000,
+  //   },
+  //   {
+  //     name: 'Bathtub',
+  //     img: 'https://images.thdstatic.com/productImages/f31d4d41-70cc-4968-a135-7a975d4f670a/svn/white-hydro-systems-corner-bathtubs-stu5959atow-64_300.jpg',
+  //     price: 1400,
+  //   },
+  //   {
+  //     name: 'Shower Head',
+  //     img: 'https://www.vieffetrade.eu/shop/foto_articoli/Hansgrohe/527023/527023.jpg',
+  //     price: 2500,
+  //   },
+  //   {
+  //     name: 'Kitchen Chrome',
+  //     img: 'https://www.ikea.com/us/en/images/products/kallsjoen-pillar-faucet-chrome-plated__0914025_pe783844_s5.jpg?f=s',
+  //     price: 2000,
+  //   },
+  //   {
+  //     name: 'Bathtub',
+  //     img: 'https://images.thdstatic.com/productImages/f31d4d41-70cc-4968-a135-7a975d4f670a/svn/white-hydro-systems-corner-bathtubs-stu5959atow-64_300.jpg',
+  //     price: 1400,
+  //   },
+  //   {
+  //     name: 'Shower Head',
+  //     img: 'https://www.vieffetrade.eu/shop/foto_articoli/Hansgrohe/527023/527023.jpg',
+  //     price: 2500,
+  //   },
 
-    {
-      name: 'Kitchen Chrome',
-      img: 'https://www.ikea.com/us/en/images/products/kallsjoen-pillar-faucet-chrome-plated__0914025_pe783844_s5.jpg?f=s',
-      price: 2000,
-    },
-    {
-      name: 'Bathtub',
-      img: 'https://images.thdstatic.com/productImages/f31d4d41-70cc-4968-a135-7a975d4f670a/svn/white-hydro-systems-corner-bathtubs-stu5959atow-64_300.jpg',
-      price: 1400,
-    },
-    {
-      name: 'Shower Head',
-      img: 'https://www.vieffetrade.eu/shop/foto_articoli/Hansgrohe/527023/527023.jpg',
-      price: 2500,
-    },
-    {
-      name: 'Bathtub',
-      img: 'https://images.thdstatic.com/productImages/f31d4d41-70cc-4968-a135-7a975d4f670a/svn/white-hydro-systems-corner-bathtubs-stu5959atow-64_300.jpg',
-      price: 1400,
-    },
-  ];
+  //   {
+  //     name: 'Kitchen Chrome',
+  //     img: 'https://www.ikea.com/us/en/images/products/kallsjoen-pillar-faucet-chrome-plated__0914025_pe783844_s5.jpg?f=s',
+  //     price: 2000,
+  //   },
+  //   {
+  //     name: 'Bathtub',
+  //     img: 'https://images.thdstatic.com/productImages/f31d4d41-70cc-4968-a135-7a975d4f670a/svn/white-hydro-systems-corner-bathtubs-stu5959atow-64_300.jpg',
+  //     price: 1400,
+  //   },
+  //   {
+  //     name: 'Shower Head',
+  //     img: 'https://www.vieffetrade.eu/shop/foto_articoli/Hansgrohe/527023/527023.jpg',
+  //     price: 2500,
+  //   },
+  //   {
+  //     name: 'Bathtub',
+  //     img: 'https://images.thdstatic.com/productImages/f31d4d41-70cc-4968-a135-7a975d4f670a/svn/white-hydro-systems-corner-bathtubs-stu5959atow-64_300.jpg',
+  //     price: 1400,
+  //   },
+  // ];
 
   pageSlice: any = [];
   //<-------- paginator event -------->
