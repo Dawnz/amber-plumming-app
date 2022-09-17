0359e32abf8ca801088ba4f1ac1322a61c49de49
dgreenbluechip <dgreen@bluechipstrategies.com.jm>
2022-09-16 23:30:12 -0500

    added route guards

---
+++ Trinity-Plumbing-App/src/app/guards/auth.guard.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  private subject = new BehaviorSubject<boolean>(true);
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (!this.subject.getValue()) {
+      this.router.navigateByUrl('/login');
+      return false;
+    }
+
+    return true;
+  }
+  constructor(public auth: AuthService, public router: Router) {
+    this.auth.isLoggedIn$.subscribe((isLoggedIn) => {
+      this.subject.next(isLoggedIn);
+    });
+  }
+}
